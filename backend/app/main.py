0581da48d4b467c371d57801aed7e5c6a86f5771
kitchenartsandletters <gil@kitchenartsandletters.com>
2025-07-21 16:02:15 -0400

    adding cors and correcting syntax

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,5 +3,13 @@ from app.routes import router as interest_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://admin.kitchenartsandletters.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Mount the interest route
 app.include_router(interest_router, prefix="/api")
